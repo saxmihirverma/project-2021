2787bb4dabc6e6addcf29c52d88e94f3c7bb2823
saxmihirverma <mihirverma19@gmail.com>
2021-06-03 14:07:35 +0530

    initial code

---
+++ project_2021/src/main/java/com/example/project_2021/Project2021Application.java
@@ -0,0 +1,13 @@
+package com.example.project_2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project2021Application.class, args);
+	}
+
+}
---
+++ project_2021/src/test/java/com/example/project_2021/Project2021ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_2021;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project2021ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
